888787278448dfbe271fb62ca5ccf4bdfa9b7418
Jerry Kurian <jkurian770@gmail.com>
2018-02-27 18:08:13 -0500

    admin basic auth fully implemented

--- app/controllers/admin/base_admin_controller.rb
+++ app/controllers/admin/base_admin_controller.rb
@@ -1,3 +1,3 @@
 class Admin::BaseAdminController < ApplicationController
-    http_basic_authenticate_with name: ENV['ADMIN_AUTH_NAME'], password: ENV["ADMIN_AUTH_PW"]
+    http_basic_authenticate_with name: ENV["ADMIN_AUTH_NAME"], password: ENV["ADMIN_AUTH_PW"]
 end
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,15 +2,12 @@ class Admin::CategoriesController < Admin::BaseAdminController
     before_filter :authorize
 
     def index
-        puts "INDEX"
         @categories = Category.order(id: :desc).all
     end
     def new 
-        puts "NEW"
         @category = Category.new
     end 
     def create
-        puts "CREATE"
         @category = Category.new(category_params)
     
         if @category.save
